385f210848a84ecea45f0f6c1a1ec6da97134f3e
Matteojw <283319098+Matteojw@users.noreply.github.com>
2026-05-10 16:02:26 +0800

    First script comes to life

---
+++ newfile.js
@@ -0,0 +1,9 @@
+console.log("--- THE DEVELOPER QUEST ---");
+
+let name = "Matteo"; 
+let status = "Level 1: Git Master";
+
+console.log("Hero: " + name);
+console.log("Current Rank: " + status);
+
+console.log("Success! You just ran your first script on your own machine.");
